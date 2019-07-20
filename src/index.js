37b580ed2d2e9018f6ea43a39d3b9f0905ad9309
Pietro Iglio <igliop@gmail.com>
2019-07-20 11:31:08 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import './style.css';
+import './style2.css';
+
+class Shape {
+  constructor (id, x, y) {
+    this.id = id
+    this.move(x, y)
+  }
+  move (x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  getArea() {
+    return this.x * this.y;
+  }
+}
+
+function component() {
+  const element = document.createElement('div');
+  let shape = new Shape(10, 10, 200);
+  
+  element.innerHTML = shape.getArea();
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+    output: {
+        filename: 'ouput.bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+        ],
+    }
+};
